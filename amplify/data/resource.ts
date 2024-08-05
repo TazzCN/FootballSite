f5936edb0d548e6c3f156691c9b3174661a92bf4
Taz Choudhury <taz@hibirdi.com>
2024-08-05 15:01:09 +0100

    adding authorisation for players

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,14 +1,16 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Player: a.model({
-    playerId: a.id(),
-    firstName: a.string(),
-    lastName: a.string(),
-    age: a.integer(),
-    position: a.string(),
-    stats: a.hasOne("PlayerStats", "playerId"),
-  }),
+  Player: a
+    .model({
+      playerId: a.id(),
+      firstName: a.string(),
+      lastName: a.string(),
+      age: a.integer(),
+      position: a.string(),
+      stats: a.hasOne("PlayerStats", "playerId"),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 
   PlayerStats: a
     .model({
